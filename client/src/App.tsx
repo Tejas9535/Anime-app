94dd40bec086d6dba00d1a834f9977d60eef0b53
Tejas Chokhawala <tchokhawala@ibaset.com>
2023-01-26 15:32:40 +0530

    added mui and cors for api model

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
 import { Button } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/home').then(function (response) {
+      // handle success
+      setData(response.data)
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }, [])
+  
 
   return (
+    <>
       <Button variant="contained">Hello World</Button>
+    </>
+    
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+  }
 
 const app = express();
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 8080;
 
-app.get('/api/scrape', async (req, res) => {
+app.get('/api/home', async (req, res) => {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://www.zoro.to');
+        await page.goto('https://www.zoro.to/home');
         const data = await page.evaluate(() => {
             return document.querySelector('body').innerText;
         });
